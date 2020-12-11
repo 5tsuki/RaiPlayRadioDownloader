627fb7e247806e6574b4c63020c9d161c7b5e1c7
Andrea <andrea.lin3110@gmail.com>
2020-12-11 11:58:54 +0100

    working

---
+++ README.md
@@ -0,0 +1,16 @@
+# RaiPlay Radio Downloader 
+Python script used to download RaiPlay Radio audio files
+## Getting Started
+To use it you just need to run the Python script from the terminal with the correct arguments.
+```
+python3 main.py -l <link> -v
+```
+### Prerequisites
+The "requirements.txt" file contains any Python dependencies. You can install them by running this command:
+```
+pip3 install -r requirements.txt
+```
+## Built With
+- [Requests](https://requests.readthedocs.io/en/master/) - HTTP library for Python.
+- [BeautifulSoup4](https://www.crummy.com/software/BeautifulSoup/bs4/doc/) - HTML documents analysis.
+- [Argparse](https://docs.python.org/3/library/argparse.html) - Parser for command-line options.
---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import argparse
+
+parser = argparse.ArgumentParser(description='Download audio files from RaiPlayRadio')
+parser.add_argument("-l", "--link", type=str, required=True, dest='link', help="Define playlist link")
+parser.add_argument("-v", "--verbose", required=False, help="Increase output verbosity", action="store_true")
+args = parser.parse_args()
+
+link = args.link
+
+if __name__ == "__main__":
+    page = requests.get(link)
+    soup = BeautifulSoup(page.content, features="html.parser")
+    elencoPlaylist = soup.find_all('li', attrs={'role':'playlist-item'})
+    for i, elemento in enumerate(elencoPlaylist):
+        currentLink = elemento.get('data-mediapolis')
+        if (args.verbose):
+            print(f"Download #{i+1}, @: {currentLink}")
+        audioFile = requests.get(currentLink)
+        open(f'{i+1}.mp3', 'wb').write(audioFile.content)
+    print("Download Completato");
